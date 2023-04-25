03f056e54c163e37c89e66fc9240db7c783a8c4e
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-04-25 15:58:10 -0300

    localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup>
 
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt
+}))
+
+watch(name, newVal => {
+    localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+    name.value = localStorage.getItem('name') || ''
+})
+
 </script>
 
 <template>
-    <div>
-        Começando projeto...
-    </div>
+    <main class="app">
+        <section class="greeting">
+            <h2 class="title">E aí, <input type="text" placeholder="seu nome..." v-model="name"></h2>
+        </section>
+    </main>
 </template>
 
