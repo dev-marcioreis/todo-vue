e2dd624ec37554f3c969f71bd734a664e94bc591
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-04-25 16:17:25 -0300

    options

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@ const todos_asc = computed(() => todos.value.sort((a, b) => {
     return a.createdAt - b.createdAt
 }))
 
+const addTodo = () => {
+
+}
+
 watch(name, newVal => {
     localStorage.setItem('name', newVal)
 })
@@ -24,7 +28,27 @@ onMounted(() => {
 <template>
     <main class="app">
         <section class="greeting">
-            <h2 class="title">E aí, <input type="text" placeholder="seu nome..." v-model="name"></h2>
+            <h2 class="title">E aí, <input type="text" placeholder="seu nome..." v-model="name" /></h2>
+        </section>
+        <section class="create-todo">
+            <h3>Criar afazeres</h3>
+            <form @submit.prevent="addTodo">
+                <h4>Qual sua lista de tarefas?</h4>
+                <input type="text" placeholder="Digite o nome..." v-model="input_content" />
+                <h4>Escolha</h4>
+                <div class="options">
+                    <label>
+                        <input type="radio" name="category" value="negócios" v-model="input_category" />
+                        <span class="bubble business"></span>
+                        <div>Negócios</div>
+                    </label>
+                    <label>
+                        <input type="radio" name="category" value="pessoal" v-model="input_category" />
+                        <span class="bubble personal"></span>
+                        <div>Pessoal</div>
+                    </label>
+                </div>
+            </form>
         </section>
     </main>
 </template>
