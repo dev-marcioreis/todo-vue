a18bfa5853426769fb3c59e903b8d386a57f827c
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-04-25 15:22:09 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dev-marcioreis/todo-vue/',
+  base: '/todo-vue/',
   plugins: [vue()],
 })
